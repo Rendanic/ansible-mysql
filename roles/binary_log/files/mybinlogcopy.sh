bd7ff5910db34160d939a6151b4ebfab64666994
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2018-01-16 06:50:35 +0100

    moved to mybinlogcopy.sh

--- roles/binary_log/files/mybinlogcopy.sh
+++ roles/binary_log/files/mybinlogcopy.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 #
-# CREATE USER 'binlogbck'@'%'  IDENTIFIED BY 'slavepass';
-# GRANT SUPER, RELOAD ON *.*  TO 'binlogbck'@'%';
+# (c) by Thorsten Bruhns (thorsten.bruhns@opitz-consulting.com)
+#
+# CREATE USER 'binlogcopy'@'localhost'  IDENTIFIED BY 'secretpassword';
+# GRANT SUPER, RELOAD ON *.*  TO 'binlogcopy'@'localhost';
+#
+# Plese define a connection with:
+# mysql_config_editor set --login-path=mybinlogcopy --host=localhost --user=mybinlogcopy -p
 #
 # - FLUSH BINARY LOGS
 #   make sure we copy the most current data
 # - gather list of BINARY LOGS
 # - cp BINARY LOGS except last one
 # - PURGE BINARY LOGS except the last one
-#
-#
 
 dest_dir=$1
-
-binlog_index=/var/lib/mysql/binary-log.index
-binlog_dir=$(dirname $binlog_index)
 login_path=mybinlogcopy
+
+setenv()
+{
+    binlog_index=/$(mysql --login-path=${login_path} --skip-column-names -B -e "SHOW  GLOBAL VARIABLES LIKE 'log_bin_index'" | cut -d"/" -f2-)
+    test -f $binlog_index || exit 10
+
+    binlog_dir=/$(dirname $(mysql --login-path=${login_path} --skip-column-names -B -e "SHOW  GLOBAL VARIABLES LIKE 'log_bin_basename'" | cut -d"/" -f2-))
+
+    if [ ! -d $binlog_dir ] ; then
+        echo "$binlog_dir not existing!"
+        exit 10
+    fi
+
+    export binlog_index binlog_dir
+}
 
 mysqlcmd()
 {
@@ -26,11 +41,8 @@ mysqlcmd()
 
 ###########################################################
 ###########################################################
+setenv
 echo "Reading binary logs from "$binlog_dir
-if [ ! -d $binlog_dir ] ; then
-    echo "$binlog_dir not existing!"
-    exit 10
-fi
 
 if [ ! -d $dest_dir ] ; then
     echo "$dest_dir not existing!"
@@ -52,5 +64,5 @@ if [ $? -eq 0 ] ; then
 fi
 
 cd ${dest_dir}
-gzip -3 ${binlog_list}
+gzip -1 ${binlog_list}
 
